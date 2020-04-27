58df9529d0bf891d9b12f8c8424e3e9c3ac93a11
elisabethkuellmer <48796369+elisabethkuellmer@users.noreply.github.com>
2020-04-27 19:48:23 +0200

    Add files via upload

---
+++ L02/main.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello World");
+console.log("Naja irgendwie ist so ein einfaches Hello World doch etwas langweilig...");
+console.log("Mhm... wie wäre es denn mit einem Witz?");
+console.log("Was sind 8 Hobbits?");
+console.log("Ein Hobbyte!");
+console.log("Wie? Nicht lustig? Ach man...");
+//# sourceMappingURL=main.js.map
---
+++ L02/main.ts
@@ -0,0 +1,6 @@
+console.log("Hello World");
+console.log("Naja irgendwie ist so ein einfaches Hello World doch etwas langweilig...");
+console.log("Mhm... wie wäre es denn mit einem Witz?");
+console.log("Was sind 8 Hobbits?");
+console.log("Ein Hobbyte!");
+console.log("Wie? Nicht lustig? Ach man...");
